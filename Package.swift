006e9a6e00f2308cf98a4cbcd4b44c46a0a22350
feather <feather@navercorp.com>
2022-03-31 19:32:05 +0900

    알라모파이어 디펜던시에 추가

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FTPrinter",
-            dependencies: [],
+            dependencies: ["Alamofire"],
             path: "Sources/Classes"),
     ]
 )
